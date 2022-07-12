2910a83e5b9fe7e5fa5447047aa8e10a2d7f936f
deluxezzz <deluxez@mail.ru>
2022-07-12 19:53:16 +0700

    burger

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var headerNav = document.querySelector('.header__nav');
+var burger = document.querySelector('.burger');
+
+if (burger){
+    burger.addEventListener("click", function(e){
+        headerNav.classList.toggle('_mobile')
+        document.body.classList.toggle('_asd');
+    });
+}
