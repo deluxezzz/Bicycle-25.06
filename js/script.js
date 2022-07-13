d5f4d95addb88cbf30eedbb7474f6d05b812d358
deluxezzz <deluxez@mail.ru>
2022-07-13 20:01:39 +0700

    правки

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-var headerNav = document.querySelector('.header__nav');
-var burger = document.querySelector('.burger');
+const headerNav = document.querySelector('.header__nav');
+const burger = document.querySelector('.burger');
 
 if (burger){
-    burger.addEventListener("click", function(e){
+    burger.addEventListener("click", function(){
         headerNav.classList.toggle('_mobile')
         document.body.classList.toggle('_lock');
     });
+}
+const navLink = document.querySelectorAll('.header__link');
+
+// if (headerNav.classList.contains('_mobile')){
+for (let i=0; i<navLink.length; i++) {
+        navLink[i].addEventListener("click", function(){
+        headerNav.classList.remove('_mobile')
+        document.body.classList.remove('_lock');
+    });
+// }
 }
